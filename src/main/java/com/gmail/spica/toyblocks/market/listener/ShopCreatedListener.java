a0aca81177e834e5a16e4b8b9f819ecc8c09035a
toyblocks <spica.toyblocks@gmail.com>
2014-09-07 16:55:41 +0900

    雑多なバグの修正(

--- src/main/java/com/gmail/spica/toyblocks/market/listener/ShopCreatedListener.java
+++ src/main/java/com/gmail/spica/toyblocks/market/listener/ShopCreatedListener.java
@@ -9,14 +9,11 @@ import static com.gmail.spica.toyblocks.market.util.FileUtil.getMarketSign;
 import static com.gmail.spica.toyblocks.market.util.FileUtil.getPlayerData;
 import static com.gmail.spica.toyblocks.market.util.FileUtil.writeMarketSign;
 import static com.gmail.spica.toyblocks.market.util.FileUtil.writePlayerData;
-import static com.gmail.spica.toyblocks.market.util.MarketSignUtil.updateStatus;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.scheduler.BukkitScheduler;
 
 public class ShopCreatedListener implements Listener {
 
@@ -28,27 +25,20 @@ public class ShopCreatedListener implements Listener {
         if (!isMarketShop(e.getSign(), true)) {
             return;
         }
-        
+
         MarketSign ms = getMarketSign(getOppSideSign(e.getSign()).getLine(3));
-        ms.upDateSign(e.getSignLine((short) 0),e.getSignLine((short) 1),e.getSignLine((short) 2),e.getSignLine((short) 3));
+        ms.upDateSign(e.getSignLine((short) 0), e.getSignLine((short) 1), e.getSignLine((short) 2), e.getSignLine((short) 3));
         PlayerData pd = getPlayerData(e.getSignLine((short) 0).replaceAll(" ", ""));
         pd.addCount(ms.type);
         writePlayerData(e.getSignLine((short) 0).replaceAll(" ", ""), pd);
         Player p = e.getPlayer();
+        ms.writeStatus(true, true);
         if (writeMarketSign(getOppSideSign(e.getSign()).getLine(3), ms)) {
             p.sendMessage("§7[§3Market§7]§aMarket連携に成功しました!");
             p.sendMessage("§7[§3Market§7]§a管理ID:§6" + getOppSideSign(e.getSign()).getLine(3));
-            BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-            scheduler.scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("Market"), new Runnable() {
-                @Override
-                public void run() {
-                    updateStatus(e.getSign());
-                }
-            }, 40L);
-            return;
+            p.sendMessage("§7§o情報の更新のために一度チェストを開閉することを推奨します");
         }
         p.sendMessage("§7[§3Market§7]§aMarket連携に失敗しました!");
         e.getSign().getBlock().breakNaturally();
-        updateStatus(e.getSign());
     }
 }
